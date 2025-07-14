e5fc3dcfa12526f6f63ad416c0ad5e06e3448236
Rifai <rifaiabdurrohman8@gmail.com>
2025-07-14 14:15:36 +0700

    initial commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // type basic
-string pesan = "Hello World!";
+string pesan = "Tahun Lahir";
 string finalPesan = pesan.ToUpper();
-Console.WriteLine(finalPesan);
-
 int tahunLahir = 2001;
 string tahunLahirr = tahunLahir.ToString();
-Console.WriteLine(tahunLahirr);
+Console.WriteLine(finalPesan + " " + tahunLahirr);
+
+int satuKM = 900;
+bool isItAKM = satuKM >= 1000;
+if (isItAKM)
+{
+  Console.WriteLine("Ini 1KM atau bahkan lebih");
+}
+
+// Division
+// int num = 2 / 3;
+//int b = 0;
+//int c = 5 / b; //Throws DevideByZeroExpection at run time;
+//int d = 5 / 0; //Compile-time error
+// Console.WriteLine(c);
+
+// Overflow
+int num = int.MinValue;
+num--;
+Console.WriteLine(num == int.MaxValue);
+int num2 = int.MaxValue;
+num2++;
+Console.WriteLine(num2 == int.MinValue);
