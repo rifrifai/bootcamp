17506ab858b578c19a9e1c3c763da8b811bcc63e
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2025-07-15 19:43:54 +0700

    initial commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// latihan foobar
+Console.WriteLine("Enter a valid number : ");
+int answer = Convert.ToInt32(Console.ReadLine());
+if (answer > 0)
+{
+  for (int i = 1; i <= answer; i++)
+  {
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+      Console.Write("foobar");
+    }
+    else if (i % 3 == 0)
+    {
+      Console.Write("foo");
+    }
+    else if (i % 5 == 0)
+    {
+      Console.Write("bar");
+    }
+    else
+    {
+      Console.Write(i);
+    }
+    if (i < answer) Console.Write(",");
+  }
+}
+else
+{
+  Console.WriteLine("Unvalid number");
+}
