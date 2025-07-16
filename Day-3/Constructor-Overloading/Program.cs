320b1360b7d5c34f64cc5bfa974673691f2d77e9
Rifai <rifaiabdurrohman8@gmail.com>
2025-07-16 11:29:36 +0700

    constructor-overloading

---
+++ Day-3/Constructor-Overloading/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// constructor overloading
+class Santri
+{
+  public int age;
+  public int tall;
+  public string? name;
+  public string? address;
+  public Santri(int age, int tall, string name, string address)
+  {
+    this.age = age;
+    this.tall = tall;
+    this.name = name;
+    this.address = address;
+    Console.WriteLine($"Santri created, name: {name}, age: {age} years old, tall: {tall} cm, address: {address}");
+  }
+  public Santri(string name, string address)
+  {
+    Console.WriteLine("Santri created");
+  }
+}
+class Program
+{
+  static void Main()
+  {
+    Santri santri1 = new(17, 180, "dzulqarnain", "demak");
+    Santri santri2 = new("sodik", "jepara");
+  }
+}
