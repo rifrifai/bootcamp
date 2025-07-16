6e170140b0d179e5eba102ee303ea77ab3dba64c
Rifai <rifaiabdurrohman8@gmail.com>
2025-07-16 14:15:09 +0700

    before-inheritance

---
+++ Day-3/A-Before-Inheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+// before inheritance
+class Santri
+{
+  // properti
+  public string? name;
+  public string? address;
+  public int age;
+  public string? hobby;
+  // method
+  public void Eat() { }
+  public void Drink() { }
+  public void Do() { }
+}
+
+class Teacher
+{
+  // properti
+  public int idTeacher;
+  public string? name;
+  public string? mapel;
+  public int age;
+  // method
+  public void Eat() { }
+  public void Drink() { }
+  public void Do() { }
+}
+
+class Cleaner
+{
+  public string? name;
+  public int age;
+  public string? tools;
+  public void Eat() { }
+  public void Drink() { }
+  public void Do() { }
+  public void Poop() { }
+}
