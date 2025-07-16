bdd4f64a705344012cfa89dedf9053901c328a30
Rifai <rifaiabdurrohman8@gmail.com>
2025-07-16 11:15:46 +0700

    costum-constructor

---
+++ Day-3/Constructor/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// constructor
+class Calculator
+{
+  // no constructor?
+  // parameterless constructor
+  public Calculator()
+  {
+    Console.WriteLine("Calculator instance created");
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    for (int i = 1; i <= 10; i++)
+    {
+      Calculator calculator = new();
+    }
+  }
+}
---
+++ Day-3/Custom-Constructor/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// constructor with parameter
+class Santri
+{
+  public int age;
+  public int tall;
+  public string? name;
+  public string? address;
+  public Santri(int age, int tall, string name, string address)
+  {
+    this.age = age;
+    this.tall = tall;
+    this.name = name;
+    this.address = address;
+  }
+}
+class Program
+{
+  static void Main()
+  {
+    Santri santri1 = new(12, 175, "rakha", "salatiga");
+    Console.WriteLine(santri1.name);
+    Console.WriteLine(santri1.address);
+    Console.WriteLine(santri1.age);
+    Console.WriteLine(santri1.tall);
+    Santri santri2 = new(13, 160, "ahmad", "kebumen");
+    Console.WriteLine(santri2.name);
+    Console.WriteLine(santri2.address);
+    Console.WriteLine(santri2.age);
+    Console.WriteLine(santri2.tall);
+  }
+}
