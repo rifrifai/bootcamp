b7f6859d9d500bb8a10b776ec373c49ec0d3949e
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2025-07-16 22:34:38 +0700

    inheritance-grandParentAnalogy

---
+++ Day-3/H-Inheritance-GrandParentAnalogy/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+// inheritance grandParent
+
+class A
+{
+  public A()
+  {
+    Console.WriteLine("A created");
+  }
+  public void Running()
+  {
+    Console.WriteLine("Running");
+  }
+}
+
+class B : A
+{
+  public B()
+  {
+    Console.WriteLine("B created");
+  }
+}
+class C : B
+{
+  public C()
+  {
+    Console.WriteLine("C created");
+  }
+}
+class D : C
+{
+  public D()
+  {
+    Console.WriteLine("D created");
+  }
+}
+class Program
+{
+  static void Main()
+  {
+    D d = new();
+    d.Running();
+  }
+}
