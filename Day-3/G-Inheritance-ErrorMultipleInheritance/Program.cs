d3060503795f3a4a0e6e77295ba99dba0461642c
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2025-07-16 22:26:18 +0700

    inheritance-errorMultipleInheritance

---
+++ Day-3/G-Inheritance-ErrorMultipleInheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+class ParentKandung
+{
+  static void Running()
+  {
+    Console.WriteLine("Parent Running");
+  }
+}
+
+class ParentAngkat
+{
+  static void Running()
+  {
+    Console.WriteLine("Parent Angkat Running");
+  }
+}
+
+class Child : ParentKandung, ParentAngka
+{
+  Child child = new();
+  child.Running();
+}
