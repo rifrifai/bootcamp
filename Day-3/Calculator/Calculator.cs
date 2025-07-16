043a879e6accb80950fd7cf9e40dc886915ffe92
Rifai <rifaiabdurrohman8@gmail.com>
2025-07-16 13:52:53 +0700

    calculator

---
+++ Day-3/Calculator/Calculator.cs
@@ -0,0 +1,8 @@
+namespace CalculatorLib;
+
+public class Calculator
+{
+  public int Add(int num1, int num2) => num1 + num2;
+  public int Devide(int num1, int num2) => num1 / num2;
+  public int Multiply(int num1, int num2) => num1 * num2;
+}
---
+++ Day-3/Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalculatorLib;
+class Program
+{
+  static void Main()
+  {
+    Calculator calculator = new();
+    // take input user from terminal
+    string userInput = Console.ReadLine() ?? "10";
+    string userInput2 = Console.ReadLine() ?? "10";
+
+    // convert string to int using parse
+    int intUserInput = Convert.ToInt32(userInput);
+    int intUserInput2 = Convert.ToInt32(userInput2);
+
+    // call calculator method
+    int result = calculator.Add(intUserInput, intUserInput2);
+    int result2 = calculator.Multiply(intUserInput, intUserInput2);
+    Console.WriteLine(result);
+    Console.WriteLine(result2);
+  }
+}
