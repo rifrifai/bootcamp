86c5a980845311a762464059888d7c1bbaca35b9
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2025-07-16 17:53:07 +0700

    inheritance-parentChildConstructor

---
+++ Day-3/D-Inheritance-ParentChildConstructor/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Animal
+{
+  public Animal()
+  {
+    Console.WriteLine("Animal Created");
+  }
+}
+class Cat : Animal
+{
+  public Cat()
+  {
+    Console.WriteLine("Cat Created");
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    Animal animal = new();
+    Cat cat = new();
+  }
+}
