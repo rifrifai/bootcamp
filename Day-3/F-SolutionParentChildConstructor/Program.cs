edc813818f58c279ef967790503ade1d44e45e6a
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2025-07-16 22:15:00 +0700

    solutionParentChildConstructor

---
+++ Day-3/F-SolutionParentChildConstructor/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// parentChild constructor
+class Component
+{
+  public string? brand;
+  public string? color;
+  public Component(string brand, string color)
+  {
+    this.brand = brand;
+    this.color = color;
+  }
+}
+
+class Bell : Component
+{
+  public int size;
+  public Bell(int size, string brand, string color) : base(brand, color)
+  {
+    this.size = size;
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    Bell bell = new(30, "honda", "black");
+    Console.WriteLine(bell.size);
+    Console.WriteLine(bell.brand);
+    Console.WriteLine(bell.color);
+  }
+}
