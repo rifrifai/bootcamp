c2d0d81f45c1d82ea58553e3bdf12272b1d27397
Rifai <rifaiabdurrohman8@gmail.com>
2025-07-16 14:54:09 +0700

    after-inheritance

---
+++ Day-3/B-After-Inheritance/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+class People
+{
+  public string? name;
+  public string? address;
+  public string? gender;
+  public int age;
+  public void Hobby() { }
+  public void Job() { }
+}
+class Santri : People
+{
+  public int idSantri;
+}
+class Teacher : People
+{
+  public int idTeacher;
+  public string? mapel;
+  public void Tools() { }
+}
+class Cleaner : People
+{
+  public int idCleaner;
+  public void Tools() { }
+}
