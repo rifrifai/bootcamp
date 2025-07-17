9975b97acf5a483635316839fcf66927a11e4415
Rifai <rifaiabdurrohman8@gmail.com>
2025-07-17 09:38:54 +0700

    encapsulationPrivate2

---
+++ Day-4/C-Encapsulation-Private2/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// private2
+public class Santri
+{
+  private int idSantri = 1100;
+  public int CheckedId(string password)
+  {
+    if (password == "p@ssw0rd")
+    {
+      return idSantri;
+    }
+    return 0;
+  }
+  public void SetId(int idBaru)
+  {
+    if (!(idBaru < 0))
+    {
+      idSantri = idBaru;
+    }
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    Santri santri = new();
+    Console.WriteLine(santri.CheckedId("p@ssw0rd"));
+    santri.SetId(2222);
+    Console.WriteLine(santri.CheckedId("p@ssw0rd"));
+  }
+}
