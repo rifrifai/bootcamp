ad19f98f955183aaa8de9a05715f31dd60ac5efa
Rifai <rifaiabdurrohman8@gmail.com>
2025-07-17 08:45:54 +0700

    encapsulationPublic

---
+++ Day-4/A-Encapsulation-Public/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// public
+class Santri
+{
+  public string? name;
+  public int age;
+  public string? hobby;
+  public void Do()
+  {
+    Console.WriteLine("Santri Mengaji");
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    Santri santri = new();
+    santri.name = "Ahmad";
+    santri.age = 22;
+    santri.hobby = "Gaming";
+    Console.WriteLine(santri.name);
+    santri.Do();
+  }
+}
