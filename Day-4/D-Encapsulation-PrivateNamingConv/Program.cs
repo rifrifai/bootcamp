4b3ab6bcd66e9776fb4d930d391c82ab62f95083
Rifai <rifaiabdurrohman8@gmail.com>
2025-07-17 09:51:46 +0700

    encapsulationPrivateNamingConv

---
+++ Day-4/D-Encapsulation-PrivateNamingConv/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Property
+{
+  private int _price;
+  public Property(int price)
+  {
+    _price = price;
+  }
+  public void Print()
+  {
+    Console.WriteLine($"the price is {_price}");
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    Property property = new(2001);
+    property.Print();
+  }
+}
