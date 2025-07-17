583bfbf63ebd86a9e08eeb1d927301ab4240eb7e
Rifai <rifaiabdurrohman8@gmail.com>
2025-07-17 08:31:33 +0700

    inheritanceCompotition but error

--- Day-3/I-InheritanceComposition/Program.cs
+++ Day-3/I-InheritanceComposition/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// inheritance compotition
+class Car
+{
+  public Car(Engine engine)
+  {
+    this.engine = engine;
+  }
+  public Engine engine;
+}
+class Engine
+{
+  public int size;
+  public string brand;
+}
+class ElectricEngine : Engine { }
+class DieselEngine : Engine { }
+class PistonEngine : Engine { }
+class Program
+{
+  static void Main()
+  {
+    Engine engine = new();
+    Car car = new(engine);
+
+    ElectricEngine electricEngine = new();
+    Car car2 = new(electricEngine);
+  }
+}
