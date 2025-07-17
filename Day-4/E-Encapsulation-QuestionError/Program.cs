c94887fe6c583ef9b6f68f1993215df214aa3d6e
Rifai <rifaiabdurrohman8@gmail.com>
2025-07-17 09:58:48 +0700

    encapsulationQuestionError

---
+++ Day-4/E-Encapsulation-QuestionError/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// it'll errror
+class Engine
+{
+  private void Run()
+  {
+    Console.WriteLine("Engine Run");
+  }
+}
+class ElectricalEngine : Engine
+{
+  public void Off()
+  {
+    Console.WriteLine("Engine Off");
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    Engine engine = new();
+    engine.Run();
+    engine.Off();
+
+    ElectricalEngine ee = new();
+    ee.Run();
+    ee.Off();
+  }
+}
