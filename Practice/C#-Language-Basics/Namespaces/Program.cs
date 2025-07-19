1443fdaf996acd80cd466378de143cc6484a0fe4
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2025-07-19 10:10:40 +0700

    namespaces

---
+++ Practice/C#-Language-Basics/Namespaces/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+
+namespace Namespace
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      WriteLine("=== C# NAMESPACES MASTERCLASS ===");
+      WriteLine("Your complete guide to organizing code like a professional\n");
+
+      BasicNamespaces();
+    }
+
+    static void BasicNamespaces()
+    {
+      WriteLine("=== BASIC NAMESPACE CONCEPTS ===");
+      WriteLine("Understanding the foundation of code organization\n");
+
+      // working with classes from our custom namespaces
+      WriteLine("--- Creating Objects from Different Namespaces ---");
+
+    }
+  }
+}
