8a83ad776a3cfe8ea84dea7d4e11538c5899e3b6
Rifai <rifaiabdurrohman8@gmail.com>
2025-07-23 11:29:10 +0700

    fix conflict

---
+++ Quiz/Logic-Exercise-2/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Enter a valid number!");
+if (int.TryParse(Console.ReadLine(), out int result))
+{
+  for (int i = 1; i <= result; i++)
+  {
+    if (i % 5 == 0 && i % 3 == 0)
+    {
+      if (i % 7 == 0)
+      {
+        Console.Write("foobarjazz, ");
+        continue;
+      }
+      Console.Write("foobar");
+    }
+    else if (i % 5 == 0)
+    {
+      if (i % 7 == 0)
+      {
+        Console.Write("foojazz, ");
+        continue;
+      }
+      Console.Write("foo");
+    }
+    else if (i % 3 == 0)
+    {
+      if (i % 7 == 0)
+      {
+        Console.Write("barjazz, ");
+        continue;
+      }
+      Console.Write("bar");
+    }
+    else
+    {
+      Console.Write(i);
+    }
+    if (i < result)
+      Console.Write(", ");
+  }
+}
+else
+{
+  Console.WriteLine("Invalid Number");
+}
