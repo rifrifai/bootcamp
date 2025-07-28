e29cf7419b8d06955a5da80aa96aed44af1d8773
Rifai <rifaiabdurrohman8@gmail.com>
2025-07-28 11:08:41 +0700

    3rd logic exercise

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,31 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-// latihan foobar
-Console.WriteLine("Enter a valid number : ");
-int answer = Convert.ToInt32(Console.ReadLine());
+﻿// logic exercise
+Console.WriteLine("Enter a valid number please!");
+int.TryParse(Console.ReadLine(), out int answer);
 if (answer > 0)
 {
   for (int i = 1; i <= answer; i++)
   {
-    if (i % 3 == 0 && i % 5 == 0)
-    {
-      Console.Write("foobar");
-    }
-    else if (i % 3 == 0)
-    {
-      Console.Write("foo");
-    }
-    else if (i % 5 == 0)
-    {
-      Console.Write("bar");
-    }
-    else
-    {
-      Console.Write(i);
-    }
-    if (i < answer) Console.Write(",");
+    string hasil = (i % 3 == 0 ? "foo" : "") +
+      (i % 5 == 0 ? "bar" : "") +
+      (i % 7 == 0 ? "jazz" : "") +
+      (i % 4 == 0 ? "baz" : "") +
+      (i % 9 == 0 ? "huzz" : "");
+
+    Console.Write(string.IsNullOrEmpty(hasil) ? i.ToString() : hasil);
+    if (i < answer) Console.Write(", ");
   }
 }
 else
 {
-  Console.WriteLine("Unvalid number");
+  Console.WriteLine("Invalid number!");
 }
