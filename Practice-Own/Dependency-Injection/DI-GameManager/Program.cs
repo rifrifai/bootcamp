5028248689a13ae89fd1189213e314edddb9fda7
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2025-08-09 16:22:20 +0700

    DI

---
+++ Practice-Own/Dependency-Injection/DI-GameManager/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace DI_GameManager;
+
+internal class Program
+{
+  static void Main(string[] args)
+  {
+    GameManager gm = new GameManager();
+
+    do
+    {
+      RoundResult result = gm.PlayGround();
+
+      if (result == RoundResult.Player1Win)
+      {
+        Console.WriteLine("Player 1 Win");
+      }
+      else if (result == RoundResult.Player2Win)
+      {
+        Console.WriteLine("Player 2 Win");
+      }
+      else
+      {
+        Console.WriteLine("It\'s a draw!");
+      }
+
+      Console.Write("Play Again (Y/N)?");
+    } while (Console.ReadLine()!.ToUpper() == "Y");
+  }
+}
