b509574a7781f22038b71cdf9f3ee632939fcacb
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2025-08-13 08:04:21 +0700

    web api

--- Practice-Own/TeddySmith/api/Program.cs
+++ Practice-Own/TeddySmith/api/Program.cs
@@ -1,11 +1,24 @@
 
+using api.Data;
+using api.Interfaces;
+using api.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IStockRepository, StockRepository>();
 
 var app = builder.Build();
 
@@ -18,6 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
 
--- Practice-Own/TeddySmith/api/models/Comment.cs
+++ Practice-Own/TeddySmith/api/models/Comment.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace api.models
+namespace api.Models
 {
     public class Comment
     {
