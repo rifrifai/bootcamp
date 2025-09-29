175dbf535f3056d63e10ae78650b596e1aeaacc3
rifaiabdurrohman <rifaiabdurrohman8@gmail.com>
2025-09-29 17:15:45 +0700

    feat : db

---
+++ Practice-Own/TeddySmith/api/Repository/PortfolioRepository.cs
@@ -0,0 +1,31 @@
+using System;
+using api.Data;
+using api.Interfaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository;
+
+public class PortfolioRepository : IPortfolioRepository
+{
+    private readonly ApplicationDBContext _context;
+    public PortfolioRepository(ApplicationDBContext context)
+    {
+        _context = context;
+    }
+    public async Task<List<Stock>> GetUserPortfolio(AppUser user)
+    {
+        var result = await _context.Portfolios.Where(u => u.AppUserId == user.Id).Select(stock => new Stock
+        {
+            Id = stock.StockId,
+            Symbol = stock.Stock.Symbol,
+            CompanyName = stock.Stock.CompanyName,
+            Purchase = stock.Stock.Purchase,
+            LastDiv = stock.Stock.LastDiv,
+            Industry = stock.Stock.Industry,
+            MarketCap = stock.Stock.MarketCap
+        }).ToListAsync();
+
+        return result;
+    }
+}
