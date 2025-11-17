8aed901cc03a65d6d28a963d13ab0b02b3be9d60
rifaiabdurrohman <rifaiabdurrohman8@gmail.com>
2025-11-17 10:42:53 +0700

    feat: add something

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,57 +5,56 @@ namespace FizzBuzzRuleGenerator;
 
 using System;
 
-public class Program
+class Program
 {
-  public static void Main()
-  {
-    Console.WriteLine("Enter a valid number please!");
-    if (int.TryParse(Console.ReadLine(), out int answer))
+    static void Main(string[] args)
     {
-      var generator = new RuleGenerator();
+        Console.WriteLine("Enter a valid number please!");
+        int.TryParse(Console.ReadLine(), out int answer);
+        if (answer > 0)
+        {
+            for (int i = 1; i <= answer; i++)
+            {
+            string hasil = (i % 3 == 0 ? "foo" : "") +
+                (i % 5 == 0 ? "bar" : "") +
+                (i % 7 == 0 ? "jazz" : "") +
+                (i % 4 == 0 ? "baz" : "") +
+                (i % 9 == 0 ? "huzz" : "");
 
-      // Configurable rules via AddRule:
-      generator.AddRule(3, "foo");
-      generator.AddRule(5, "bar");
-      generator.AddRule(7, "jazz");
-      generator.AddRule(4, "baz");
-      generator.AddRule(9, "huzz");
+            Console.Write(string.IsNullOrEmpty(hasil) ? i.ToString() : hasil);
+            if (i < answer) Console.Write(", ");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid number!");
+        }
 
-      // Run the generator
-      generator.Generate(answer);
     }
-    else
-    {
-      Console.WriteLine("Invalid number!");
-    }
-  }
 }
 
-
-// class Program
+// public class Program
 // {
-//   static void Main(string[] args)
+//   public static void Main()
 //   {
 //     Console.WriteLine("Enter a valid number please!");
-//     int.TryParse(Console.ReadLine(), out int answer);
-//     if (answer > 0)
+//     if (int.TryParse(Console.ReadLine(), out int answer))
 //     {
-//       for (int i = 1; i <= answer; i++)
-//       {
-//         string hasil = (i % 3 == 0 ? "foo" : "") +
-//           (i % 5 == 0 ? "bar" : "") +
-//           (i % 7 == 0 ? "jazz" : "") +
-//           (i % 4 == 0 ? "baz" : "") +
-//           (i % 9 == 0 ? "huzz" : "");
+//       var generator = new RuleGenerator();
 
-//         Console.Write(string.IsNullOrEmpty(hasil) ? i.ToString() : hasil);
-//         if (i < answer) Console.Write(", ");
-//       }
+//       // Configurable rules via AddRule:
+//       generator.AddRule(3, "foo");
+//       generator.AddRule(5, "bar");
+//       generator.AddRule(7, "jazz");
+//       generator.AddRule(4, "baz");
+//       generator.AddRule(9, "huzz");
+
+//       // Run the generator
+//       generator.Generate(answer);
 //     }
 //     else
 //     {
 //       Console.WriteLine("Invalid number!");
 //     }
-
 //   }
 // }
